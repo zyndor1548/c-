6fd3059cd29d9af6ddec9b45e12e6a97be5720d1
zyndor1548 <a.s.aromal2006@gmail.com>
2024-10-21 23:10:42 +0530

    can convert numbers to words under thousand

--- number to word.cpp
+++ number to word.cpp
@@ -2,8 +2,8 @@
 #include <string>
 using namespace std;
 
-string getdigit(int digit){
-    switch(digit){
+string number(int num){
+        switch(num){
         case 1: return "one";
         case 2: return "two";
         case 3: return "three";
@@ -14,51 +14,63 @@ string getdigit(int digit){
         case 8: return "eight";
         case 9: return "nine";
         default:
-                break;
+                return "";
+        }
+}
+
+string tens(int num){
+    switch(num){
+        case 1: return "fuck shit";
+        case 2: return "twenty";
+        case 3: return "thirty";
+        case 4: return "forty";
+        case 5: return "fifty";
+        case 6: return "sixty";
+        case 7: return "seventy";
+        case 8: return "eighty";
+        case 9: return "ninenty";
+        default:
+                return "";
     }
 }
 
-string getposition(int digit,int pos){
+string getdigit(string digit){
+    int num,pos;
     string position;
-    switch(pos){
-        case 12:
-        case 11:
-        case 10:
-                position += getdigit(digit) + " ";
-                position += "billion";
-                break;
-        case 9:
-        case 8:
-        case 7:
-                position += getdigit(digit) + " ";
-                position += "million";
-                break;
-        case 6:
-        case 5:
-        case 4:
-                position += getdigit(digit) + " ";
-                position += "thousand";
-                break;
-        case 3:
-                position += getdigit(digit) + " ";
-                position += "hundred";
-                break;
-        default:
-            break;       
+    for( size_t i = 0; i < digit.size() ; i++){
+        num = digit.at(i) - '0';
+        pos = digit.size() - i;
+        if (pos == 3){
+            position += number(num) + " ";
+            position += "hundred ";
+        }else if(pos == 1){
+            position += number(num) + " ";
+        }else{
+            position += tens(num) + " ";
+        }    
     }
     return position;
 }
 
 int main(){
 
-    long long number = 9900000000,digit,pos;
-    string numberStr = to_string(number),word;
-    for( size_t i = 0; i < numberStr.size() ; i++){
-        digit = numberStr.at(i) - '0';
-        pos = numberStr.size() - i;
-        if (digit != 0) {
-            word += getposition(digit, pos) + " ";
-        }
+    long long number = 888;
+    int length,cut;
+    string word;
+    string numberstr = to_string(number);
+    length = numberstr.size();
+    cut = length % 3;
+    int numgroups =( cut > 0 ) ? ( length / 3 + 1 ) : ( length / 3 );
+    string group[numgroups];
+    int index = 0;
+    if ( cut > 0 ){
+        group[index++] = numberstr.substr( 0 , cut );
+    }
+    for( int i = cut ; i < length ; i += 3){
+        group[index++] = numberstr.substr( i , 3 );
+    }
+    for(int i = 0 ;i < index ;i++){
+       word += getdigit(group[i]) + " ";  
     }
     cout<<word;
 
